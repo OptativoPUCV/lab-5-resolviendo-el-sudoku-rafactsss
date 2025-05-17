effbea196822a15ccaf4c279fc9673a0acdfcf6b
Your Name <you@example.com>
2025-05-17 15:07:03 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,7 +44,8 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-   int seen[10];
+    int i, j, k, l;
+    int seen[10];
     // Revisar filas
     for(i = 0; i < 9; i++){
         for(k = 1; k <= 9; k++) seen[k] = 0;
